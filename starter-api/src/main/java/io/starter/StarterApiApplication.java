dd6abd73077ccd7c54f7ca034873a6a75a081645
HyunTaek Oh <oht222@naver.com>
2022-02-15 23:45:21 +0900

    Initial commit

---
+++ starter-api/src/main/java/io/starter/StarterApiApplication.java
@@ -0,0 +1,12 @@
+package io.starter;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApiApplication.class, args);
+    }
+}
---
+++ starter-web/src/main/java/io/starter/StarterWebApplication.java
@@ -0,0 +1,11 @@
+package io.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StarterWebApplication.class, args);
+    }
+}
