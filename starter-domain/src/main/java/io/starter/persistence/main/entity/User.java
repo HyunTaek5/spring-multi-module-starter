06155ef69f9b88e1d716659b183dcaefa968d5bc
HyunTaek Oh <oht222@naver.com>
2022-04-03 15:10:35 +0900

    Idle Entity deleted Add id, createdAt, updatedAt to User Entity

--- starter-domain/src/main/java/io/starter/persistence/main/entity/User.java
+++ starter-domain/src/main/java/io/starter/persistence/main/entity/User.java
@@ -3,12 +3,30 @@ package io.starter.persistence.main.entity;
 import io.starter.persistence.main.entity.type.Role;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
 
 @Entity @Getter @Setter
-public class User extends Idle{
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private Long id;
+
+    @CreatedDate
+    @Column(name="createdAt")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name="updatedAt")
+    private Date updatedAt;
+
     @Column(name="email")
     private String email;
 
