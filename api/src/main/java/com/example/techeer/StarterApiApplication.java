b3acfcbc67c7469cb56179e2f8daecbaf3c2ba37
HyunTaek Oh <oht222@naver.com>
2023-03-05 01:19:18 +0900

    fix: JPA 어노테이션 추가 및 디렉토리 수정

--- api/src/main/java/com/example/techeer/StarterApiApplication.java
+++ api/src/main/java/com/example/techeer/StarterApiApplication.java
@@ -1,10 +1,11 @@
-package io.starter;
-
+package com.example.techeer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StarterApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(StarterApiApplication.class, args);
